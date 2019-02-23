ae5ddb411ba4c242dcf18eda8dd64fe2d476180e
John Douglas Taiwan de Sousa Costa <johnsousa61@gmail.com>
2019-02-23 12:34:38 -0300

    Enum DiaSemana finalizado

---
+++ src/br/com/estudo/enumeradores/DiaSemana.java
@@ -0,0 +1,15 @@
+package br.com.estudo.enumeradores;
+
+public enum DiaSemana {
+    //Os argumentos são os valores que cada CONSTANTE enum possui.
+    SEGUNDA(1), TERCA(2), QUARTA(3), QUINTA(4), SEXTA(5), SABADO(6), DOMINGO(7);
+
+    int dia =0;
+
+    DiaSemana(int dia) {
+        if (dia < 1 || dia > 7){
+            throw new IllegalArgumentException("Dia da semana fora do intervalo permitido");
+        }
+        this.dia = dia;
+    }
+}
