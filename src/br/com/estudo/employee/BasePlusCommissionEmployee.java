f8d78ce59df327d098665f061091605a3d540bd5
John Douglas Taiwan de Sousa Costa <johnsousa61@gmail.com>
2019-02-28 10:36:17 -0300

    Clase BasePlusCommisisonEmployee finalizada utilizando composition

--- src/br/com/estudo/employee/BasePlusCommissionEmployee.java
+++ src/br/com/estudo/employee/BasePlusCommissionEmployee.java
@@ -1,4 +1,37 @@
 package br.com.estudo.employee;
 
+//Using composition.
+
 public class BasePlusCommissionEmployee {
+    private double baseSalaty;
+    private CommissonEmployee commissonEmployee;
+
+    public BasePlusCommissionEmployee(double baseSalaty, CommissonEmployee commissonEmployee) {
+        if (baseSalaty < .0) {
+            throw new IllegalArgumentException("Base salary most be >= 0.0.");
+        }
+
+        this.baseSalaty = baseSalaty;
+        this.commissonEmployee = commissonEmployee;
+    }
+
+    public double getBaseSalaty() {
+        if (baseSalaty < .0) {
+            throw new IllegalArgumentException("Base salary most be >= 0.0");
+        }
+        return baseSalaty;
+    }
+
+    public void setBaseSalaty(double baseSalaty) {
+        this.baseSalaty = baseSalaty;
+
+    }
+
+    public CommissonEmployee getCommissonEmployee() {
+        return commissonEmployee;
+    }
+
+    public void setCommissonEmployee(CommissonEmployee commissonEmployee) {
+        this.commissonEmployee = commissonEmployee;
+    }
 }
